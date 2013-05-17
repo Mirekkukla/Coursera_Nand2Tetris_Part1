a485a275e3b7ca1e099ce30c0cec753d6189575e
Mirek Kukla <mirek@addepar.com>
2013-05-17 10:14:09 -0700

    Assignment 4 working

---
+++ src/assignment4/Board.java
@@ -0,0 +1,202 @@
+public class Board {
+  private final int dim;
+  private final char[][] blocks;
+  private final int hamming;
+  private final int manhattan;
+  private final int blank = 0;
+  private int emptyI; // i coord of empty block
+  private int emptyJ; // j coord of empty block
+
+  // construct a board from an N-by-N array of ints
+  public Board(int[][] blocks) {
+    dim = blocks.length;
+    this.blocks = new char[dim][dim];
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        this.blocks[i][j] = (char) blocks[i][j];
+        if (blocks[i][j] == 0) {
+          emptyI = i;
+          emptyJ = j;
+        }
+      }
+    }
+    hamming = getHammingScore();
+    manhattan = getManhattanScore();
+  }
+
+  // the length of the side of the board
+  public int dimension() {
+    return dim;
+  }
+
+  // number of blocks out of place
+  public int hamming() {
+    return hamming;
+  }
+
+  // sum of Manhattan distances between blocks and goal
+  public int manhattan() {
+    return manhattan;
+  }
+
+  // is this board the goal board?
+  public boolean isGoal() {
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        if (i == dim - 1 && j == dim - 1) continue; //don't check bottom right, should be zero
+        if (blocks[i][j] != i * dim + (j + 1)) return false;
+      }
+    }
+    return true;
+  }
+
+  // a board obtained by exchanging two adjacent blocks in the same row
+  public Board twin() {
+    int twinBlocks[][] = blockIntCopy(blocks);
+    int swapRow; //make sure we're not swapping a block with a blank
+    if (blocks[0][0] != 0 && blocks[0][1] != 0) swapRow = 0;
+    else swapRow = 1;
+    twinBlocks[swapRow][0] = blocks[swapRow][1];
+    twinBlocks[swapRow][1] = blocks[swapRow][0];
+    return new Board(twinBlocks);
+  }
+
+  @Override
+  public boolean equals(Object y) {
+    if (y == this) return true;
+    if (y == null) return false;
+    if (y.getClass() != this.getClass()) return false;
+    if (this.dim != ((Board) y).dim) return false;
+    if (this.manhattan != ((Board) y).manhattan || this.hamming != ((Board) y).hamming) return false;
+    return this.toString().equals(y.toString());
+  }
+
+  // returns stack of all neighboring boards
+  public Iterable<Board> neighbors() {
+    Stack<Board> stack = new Stack<Board>();
+
+    for (int i_shift = -1; i_shift <= 1; i_shift++) {
+      for (int j_shift = -1; j_shift <= 1; j_shift++) {
+        if (Math.abs(i_shift) - Math.abs(j_shift) != 0
+            && isLegalLoc(emptyI + i_shift, emptyJ + j_shift)) {
+          int[][] newBlocks = blockIntCopy(blocks);
+          newBlocks[emptyI][emptyJ] = blocks[emptyI + i_shift][emptyJ + j_shift];
+          newBlocks[emptyI + i_shift][emptyJ + j_shift] = 0;
+          Board neighbor = new Board(newBlocks);
+          stack.push(neighbor);
+        }
+      }
+    }
+    return stack;
+  }
+
+  // takes array of chars, returns array cast to ints
+  // which we're assuming has the same dims as this board
+  private int[][] blockIntCopy(char[][] oldBlocks) {
+    int[][] newBlocks = new int[dim][dim];
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        newBlocks[i][j] = oldBlocks[i][j];
+      }
+    }
+    return newBlocks;
+  }
+
+  // first line gives the dimension (length of a side)
+  // then, return on line per row
+  @Override
+  public String toString() {
+    StringBuilder s = new StringBuilder();
+    s.append(dim + "\n");
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        s.append(String.format("%2d ", blocks[i][j]));
+      }
+      s.append("\n");
+    }
+    return s.toString();
+  }
+
+  // this returns the hamming SCORE, not the hamming priority function
+  private int getHammingScore() {
+    int count = 0;
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        if ((blocks[i][j] != blank) && (blocks[i][j] != numFromIJCoord(i, j))) count++;
+      }
+    }
+    return count;
+  }
+
+  // see getHamingScore comment
+  private int getManhattanScore() {
+    int count = 0;
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        int num = blocks[i][j];
+        if (num != blank)
+          count += Math.abs(iCoordFromNum(num) - i) + Math.abs(jCoordFromNum(num) - j);
+      }
+    }
+    return count;
+  }
+
+  // num starts at one, which corresponds to top left of board
+  // num increases left to right, top to bottom.
+  // 1 2 3        (0,0) (0,1) (0,2)
+  // 4 5 6   <->  (1,0) (1,1) (1,2)
+  // 7 8 9        (2,0) (2,1) (2,2)
+  private int iCoordFromNum(int num) {
+    return (num - 1) / dim;
+  }
+
+  // see iCoordFromNum
+  private int jCoordFromNum(int num) {
+    return (num - 1) % dim;
+  }
+
+  // see iCoordFromNum
+  private int numFromIJCoord(int i, int j) {
+    return i * dim + (j + 1);
+  }
+
+  // where top left <-> (0,0), i is row, j is column
+  private boolean isLegalLoc(int i, int j) {
+    if (i < 0 || i >= dim || j < 0 || j >= dim)
+      return false;
+    else
+      return true;
+  }
+
+  // unit tests
+  public static void main(String[] args) {
+//    int size = 3;
+//    int blocks[][] = new int[size][size];
+//    blocks[0][0] = 1;
+//    blocks[0][1] = 2;
+//    blocks[0][2] = 3;
+//    blocks[1][0] = 4;
+//    blocks[1][1] = 5;
+//    blocks[1][2] = 6;
+//    blocks[2][0] = 7;
+//    blocks[2][1] = 8;
+//    blocks[2][2] = 0;
+//
+//    Board doneB = new Board(blocks);
+//    if (doneB.isGoal() == false) System.out.println("FAIL");
+//    if (doneB.getManhattanScore() != 0) System.out.println("FAIL");
+//    if (doneB.getHammingScore() != 0) System.out.println("FAIL");
+//    System.out.println("Done board");
+//    System.out.println(doneB.toString());
+//
+//    Board twinB = doneB.twin();
+//    System.out.println("Twin board");
+//    System.out.println(twinB.toString());
+//
+//    System.out.println("Neighbor boards");
+//    for (Board board : doneB.neighbors()) {
+//      System.out.println(board.toString());
+//    }
+
+  }
+}
---
+++ src/assignment4/Solver.java
@@ -0,0 +1,110 @@
+public class Solver {
+  private pBoard solution = null;
+  private final MinPQ<pBoard> pBoardQ = new MinPQ<pBoard>();
+  private final MinPQ<pBoard> twinPBoardQ = new MinPQ<pBoard>();
+
+  // find a solution to the initial board (using the A* algorithm)
+  // search for solution on twin board at the same time. If the twin
+  // board finds a solution, then we know one doesn't exist for the original
+  public Solver(Board initial) {
+    pBoardQ.insert(new pBoard(initial, null, 0));
+    twinPBoardQ.insert(new pBoard(initial.twin(), null, 0));
+
+    while (true) {
+      if (pBoardQ.isEmpty()) break;
+      pBoard currentPBoard = pBoardQ.delMin();
+      pBoard currentTwinBoard = twinPBoardQ.delMin();
+
+      if (currentPBoard.board.isGoal()) {
+        solution = currentPBoard;
+        break;
+      } else if (currentTwinBoard.board.isGoal()) { //If the twin is solvable, the original isn't
+        break;
+      }
+
+      insertNeighbors(pBoardQ, currentPBoard);
+      insertNeighbors(twinPBoardQ, currentTwinBoard);
+    }
+  }
+
+  // insert all neighbors of pboard into the priority queue, except for
+  // the neighbor that was the parent of pboard
+  private void insertNeighbors(MinPQ<pBoard> queue, pBoard pboard) {
+    for (Board neighborBoard : pboard.board.neighbors()) {
+      if (neighborBoard.equals(pboard.board)) {
+        continue;
+      }
+      queue.insert(new pBoard(neighborBoard, pboard, pboard.moves + 1));
+    }
+  }
+
+  // a Board with a pointer to its ancestor in the priority queue, as well as a priority
+  private class pBoard implements Comparable<pBoard> {
+    private final Board board;
+    private final pBoard parent;
+    private final int priority;
+    private final int moves;
+
+    private pBoard(Board board, pBoard parent, int moves) {
+      this.board = board;
+      this.parent = parent;
+      this.moves = moves;
+      this.priority = board.manhattan() + moves;
+    }
+
+    @Override
+    public int compareTo(pBoard other) {
+      return this.priority - other.priority;
+    }
+  }
+
+  // is the initial board solvable? //TODO: DO THETWO BOARD THING
+  public boolean isSolvable() {
+    if (solution == null) return false;
+    else return true;
+  }
+
+  public int moves() { // min number of moves to solve initial board; -1 if no solution
+    if (solution == null) return -1;
+    else return solution.moves;
+  }
+
+  // sequence of boards in a shortest solution; null if no solution
+  public Iterable<Board> solution() {
+    if (solution == null) return null;
+    Stack<Board> stack = new Stack<Board>();
+    stack.push(solution.board);
+    pBoard parentPBoard = solution.parent;
+    while (parentPBoard != null) {
+      stack.push(parentPBoard.board);
+      parentPBoard = parentPBoard.parent;
+    }
+    return stack;
+  }
+
+  public static void main(String[] args) { // solve a slider puzzle (given below)
+    // create initial board from file
+    In in = new In(args[0]);
+    int N = in.readInt();
+    int[][] blocks = new int[N][N];
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < N; j++)
+        blocks[i][j] = in.readInt();
+    Board initial = new Board(blocks);
+
+    System.out.println("We're about to solve:");
+    System.out.println(initial.toString());
+
+    // solve the puzzle
+    Solver solver = new Solver(initial);
+
+    // print solution to standard output
+    if (!solver.isSolvable())
+      StdOut.println("No solution possible");
+    else {
+      StdOut.println("Minimum number of moves = " + solver.moves());
+      for (Board board : solver.solution())
+        StdOut.println(board);
+    }
+  }
+}
