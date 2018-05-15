5e4b0d9a7b0954b422e6bbbff48e30ecd50f6732
Mirek Kukla <mirekkukla@gmail.com>
2018-05-15 16:06:30 +0200

    Create intellij project, update gitignore

--- HW1/src/com/ninjoncrack/crypto/hw1/TxHandler.java
+++ HW1/src/com/ninjoncrack/crypto/hw1/TxHandler.java
@@ -1,8 +1,10 @@
+package com.ninjoncrack.crypto.hw1;
+
 public class TxHandler {
 
     /**
-     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
-     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * Creates a public ledger whose current com.ninjoncrack.crypto.hw1.UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the com.ninjoncrack.crypto.hw1.UTXOPool(com.ninjoncrack.crypto.hw1.UTXOPool uPool)
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
@@ -11,24 +13,26 @@ public class TxHandler {
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+     * (1) all outputs claimed by {@code tx} are in the current com.ninjoncrack.crypto.hw1.UTXO pool,
      * (2) the signatures on each input of {@code tx} are valid, 
-     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (3) no com.ninjoncrack.crypto.hw1.UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
+        return false;
     }
 
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
      * transaction for correctness, returning a mutually valid array of accepted transactions, and
-     * updating the current UTXO pool as appropriate.
+     * updating the current com.ninjoncrack.crypto.hw1.UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+        return null;
     }
 
 }
