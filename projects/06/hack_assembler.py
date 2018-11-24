3f3fb8a89b9e99d4c151c56f0422d7af54435680
Mirek Kukla <mirekkukla@gmail.com>
2018-11-24 10:15:28 +0100

    cleanup

--- projects/06/hack_assembler.py
+++ projects/06/hack_assembler.py
@@ -49,11 +49,37 @@ COMP_MAP_WITH_M = {
     'M+1': '110111',
     'M-1': '110010',
     'D+M': '000010',
-    'M+D': '000010', # added this cause my proj 4 code uses it, technically not part of spec
+    'M+D': '000010', # technically not part of spec
     'D-M': '010011',
     'M-D': '000111',
     'D&M': '000000',
     'D|M': '010101'
+}
+
+SYMBOL_CONSTANTS = {
+    'SP': 0,
+    'LCL': 1,
+    'ARG': 2,
+    'THIS': 3,
+    'THAT': 4,
+    'R0': 0,
+    'R1': 1,
+    'R2': 2,
+    'R3': 3,
+    'R4': 4,
+    'R5': 5,
+    'R6': 6,
+    'R7': 7,
+    'R8': 8,
+    'R9': 9,
+    'R10': 10,
+    'R11': 11,
+    'R12': 12,
+    'R13': 13,
+    'R14': 14,
+    'R15': 15,
+    'SCREEN': 16384,
+    'KBD': 24576
 }
 
 def main():
@@ -67,7 +93,7 @@ def main():
     lines = load_file(file_path_str)
     clean_lines = cleanup_lines(lines)
 
-    symbol_table = get_initial_symbol_table()
+    symbol_table = {symbol : address for symbol, address in SYMBOL_CONSTANTS.iteritems()}
     label_free_lines = extract_labels(clean_lines, symbol_table)
 
     converted_lines = []
@@ -90,34 +116,6 @@ def main():
 
     export_to_file(file_path_str, dest_folder_path_str, converted_lines)
     print "ALL DONE"
-
-# not a contant since we're going to be appending to it
-def get_initial_symbol_table():
-    return {
-        'SP': 0,
-        'LCL': 1,
-        'ARG': 2,
-        'THIS': 3,
-        'THAT': 4,
-        'R0': 0,
-        'R1': 1,
-        'R2': 2,
-        'R3': 3,
-        'R4': 4,
-        'R5': 5,
-        'R6': 6,
-        'R7': 7,
-        'R8': 8,
-        'R9': 9,
-        'R10': 10,
-        'R11': 11,
-        'R12': 12,
-        'R13': 13,
-        'R14': 14,
-        'R15': 15,
-        'SCREEN': 16384,
-        'KBD': 24576
-    }
 
 
 def extract_labels(lines, symbol_table):
@@ -146,7 +144,7 @@ def extract_labels(lines, symbol_table):
 
 
 def load_file(file_path_str):
-    """ Load file with given ".asm" filename, return list of lines"""
+    """ Load file with given ".asm" filename, return list of lines """
     if not file_path_str.split('.')[-1] == "asm":
         raise Exception("filename has to end in '.asm', you gave '{}'".format(file_path_str))
 
@@ -176,7 +174,7 @@ def convert_a_instruction(line):
     Convert the given a-instruction into its HACK machine language representation
     Instruction should start with a "@" followed exclusively by digits
     Return the resulting string of 1s and 0s (of length 16)
-    EX: if input line = '@8', we'll return '0000000000001000'
+    EX: '@8' becomes '0000000000001000'
     """
     number_str = line[1:]
     if line[0] != '@' or not number_str.isdigit():
@@ -190,7 +188,7 @@ def convert_symbol(line, symbol_table, next_address_to_allocate):
     Convert the given symbol line into a-instruction using the given symbol table
     Return a tuple of (converted_instruction, added_to_table), where the later is a boolean
     that tells us whether or not we had to add the symbol to our symbol table
-    EX: if input line = '@SCREEN', we'll return ('0100000000000000', False)
+    EX: if line = '@SCREEN', we'll return ('0100000000000000', False)
     """
     symbol = line[1:]
     if line[0] != '@' or symbol.isdigit():
@@ -212,7 +210,7 @@ def convert_c_instruction(line):
     """
     Convert the given c-instruction into its HACK machine language representation
     Return the resulting string of 1s and 0s (of length 16)
-    EX:  XXXXXXXXXXXXXXXXXX
+    EX:  'D=M;JMP' becomes '1111110000010111'
     """
     jump_bits = get_jump_bits(line)
     dest_bits = get_dest_bits(line)
@@ -257,7 +255,7 @@ def get_comp_bits(line):
 
 
 def get_padded_bin_string(int_value):
-    """ EX: if int_value = 8, we'll return "0000000000001000" """
+    """ EX: if int_value = 8, we'll return '0000000000001000' """
     if not isinstance(int_value, int):
         raise Exception("Can only convert ints to binary: {}".format(int_value))
 
